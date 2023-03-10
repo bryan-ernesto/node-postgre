dd1165ee5b07d14f5fc1bbdae6c697e2f6551a2b
Bryan Gamez <bgamez@groupnova.com.gt>
2023-03-10 10:45:39 -0600

    Add categories

--- middlewares/auth.handler.js
+++ middlewares/auth.handler.js
@@ -1,6 +1,6 @@
 const boom = require('@hapi/boom');
 
-const { config } = require('./../config/config')
+const { config } = require('./../config/config');
 
 function checkApiKey(req, res, next) {
   const apiKey = req.headers['api'];
@@ -11,4 +11,14 @@ function checkApiKey(req, res, next) {
   }
 }
 
-module.exports = { checkApiKey };
+function checkAdminRole(req, res, next) {
+  console.log(req.user);
+  const user = req.user;
+  if (user.role === 'admin') {
+    next();
+  } else {
+    next(boom.unauthorized());
+  }
+}
+
+module.exports = { checkApiKey, checkAdminRole };
